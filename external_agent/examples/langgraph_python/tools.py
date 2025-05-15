9dbd711585ebc5ff908ab05b38d953f0bc0ee5b8
arvindrajpurohit23 <149211469+arvindrajpurohit23@users.noreply.github.com>
2025-05-15 11:39:39 -0400

    Update tools.py

--- external_agent/examples/langgraph_python/tools.py
+++ external_agent/examples/langgraph_python/tools.py
@@ -15,7 +15,42 @@ def news_search_duckduckgo(search_phrase: str):
     results = search.run(search_phrase) 
     return results
 
+@tool
+#def langflow(search_phrase: str) -> dict:
+def langflow(search_phrase: str):
+    """Search recipes using langflow ."""
+    url = f"https://api.langflow.astra.datastax.com/XXXXXXXXXXXXXXXX"    # api url of langflow project
+    # Request payload configuration
+    payload = {
+        "input_value": search_phrase ,  # The input value to be processed by the flow
+        "output_type": "chat",  # Specifies the expected output format
+        "input_type": "chat"  # Specifies the input format
+    }
+    # Request headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer LANGFLOW-BEARER-TOKEN"  # Authentication key from environment variable'}
+    }
+
+    #print(search_phrase, "This is the INPUT for Langflow: ")
+
+    try:
+        # Send API request
+        response = requests.request("POST", url, json=payload, headers=headers)
+        response.raise_for_status()  # Raise exception for bad status codes
+
+        # Print response
+        #print(response.text, "This is the OUTPUT of Langflow: ")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error making API request: {e}")
+    except ValueError as e:
+        print(f"Error parsing response: {e}")
+    
+    return response
+
 tool_choices = {
     "web_search_duckduckgo": web_search_duckduckgo,
     "news_search_duckduckgo": news_search_duckduckgo,
+    "langflow": langflow,
 }
